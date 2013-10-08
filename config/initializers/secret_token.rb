51b808e3eec7de0bd95e17cc4026700d0a04c50d
Sri Kavuru <skavuru18@gmail.com>
2013-10-07 19:54:17 -0700

    Created a rails application and added input field for status.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ngpkv::Application.config.secret_token = 'dff47ecb1e561423d0b83b16a981fc3789bf8b272da8aa31adb1f322895646dcd61b7ab618dadf19edcaab12d5f6a0abe7e8cdb28f96a6c7f2a10a3c77dc0511'
